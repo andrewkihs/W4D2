82975aa2cd75b5d23f45746ba47df7c472abe722
ShenQi1996 <qisam1989@gmail.com>
2021-03-17 12:10:15 -0400

    done with Rook

--- chess/pieces/Rook.rb
+++ chess/pieces/Rook.rb
@@ -1,18 +1,19 @@
 require_relative '../piece.rb'
 require_relative '../modules/slideable.rb'
 
-class Bishop < Piece
+class Rook < Piece
   include Slideable
 
   def symbol
-    '♝'.colorize(color)
+    '♜'.colorize(color)
   end
 
   protected
 
   def move_dirs
     #ONLY MOVES DIAGONAL
-    diagonal_dirs
+    horizontal_dirs
+    #horizontal_dirs
     # return the directions in which a bishop can move
     # a bishop can move diagonally
   end
